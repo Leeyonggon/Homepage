3bf5def83209356936fe15b8188dc845191ea310
leeyonggon <gong1752@icloud.com>
2024-01-03 17:08:11 +0900

    1

---
+++ lib/screens/Introduce.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Introduce extends StatefulWidget {
+  const Introduce({super.key});
+
+  @override
+  State<Introduce> createState() => _IntroduceState();
+}
+
+class _IntroduceState extends State<Introduce> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
