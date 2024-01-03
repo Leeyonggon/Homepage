e8da955a00ecb732182250e7a1ee9cdf46e46b2a
leeyonggon <gong1752@icloud.com>
2024-01-03 15:26:53 +0900

    first

---
+++ lib/screen1.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class MScreen extends StatelessWidget {
+  const MScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: const Text('첫 시작'),
+      ),
+    );
+  }
+}
