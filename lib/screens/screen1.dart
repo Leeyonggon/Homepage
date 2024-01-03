37f4b8da1f9f2890eb4d75f099973955411165ae
leeyonggon <gong1752@icloud.com>
2024-01-03 15:54:40 +0900

    초안

---
+++ lib/screens/screen1.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+late double height;
+late double width;
+
+class MScreen extends StatelessWidget {
+  const MScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    height = MediaQuery.of(context).size.height;
+    width = MediaQuery.of(context).size.width;
+
+    return const Scaffold(
+      backgroundColor: Colors.black,
+      body: Stack(
+        alignment: Alignment.topCenter,
+        children: [],
+      ),
+    );
+  }
+}
